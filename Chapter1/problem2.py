da6295886efbf15ac48f8fece6c8d42b892fad43
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-15 18:18:43 +0500

    Update problem2.py

--- Chapter1/problem2.py
+++ Chapter1/problem2.py
@@ -1,12 +1,13 @@
 # Install an external module and use it to perform an operation of your interest
+# commit 1 to run number 2 and vice versa these both are different modules 
 
 # 1
-# import pyttsx3
-# engine = pyttsx3.init()
-# engine.say("Run Run ")
-# engine.runAndWait()
+ import pyttsx3
+ engine = pyttsx3.init()
+ engine.say("Run Run ")
+ engine.runAndWait()
 
 # 2
 
-# from emoji import emojize 
-# print(emojize(":thumbs_up:")) 
+ from emoji import emojize 
+ print(emojize(":thumbs_up:")) 
