819d70fd733bd78f11e61df645c161d15e3ea74e
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-15 18:17:41 +0500

    Update problem2.py

--- Chapter1/problem2.py
+++ Chapter1/problem2.py
@@ -1,15 +1,12 @@
+# Install an external module and use it to perform an operation of your interest
+
+# 1
 # import pyttsx3
 # engine = pyttsx3.init()
 # engine.say("Run Run ")
 # engine.runAndWait()
 
-# next
+# 2
 
 # from emoji import emojize 
 # print(emojize(":thumbs_up:")) 
-
-# 2 
-
-import emoji
-emojified = emoji.encode("There is a :snake: in my boot !") 
-print(emojified) 
