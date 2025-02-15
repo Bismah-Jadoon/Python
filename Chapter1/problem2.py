4cdc4e8a84c4ca6312dd948cea8cd963b99b26f8
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-15 12:46:15 +0500

    Add files via upload

---
+++ Chapter1/problem2.py
@@ -0,0 +1,15 @@
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("Run Run ")
+# engine.runAndWait()
+
+# next
+
+# from emoji import emojize 
+# print(emojize(":thumbs_up:")) 
+
+# 2 
+
+import emoji
+emojified = emoji.encode("There is a :snake: in my boot !") 
+print(emojified) 
