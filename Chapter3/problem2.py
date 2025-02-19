d8cb82ef54b316de9bbdb07f886c6c8d5a6c1c8c
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-19 09:22:42 +0500

    Update problem2.py

--- Chapter3/problem2.py
+++ Chapter3/problem2.py
@@ -1,5 +1,6 @@
+# Replace Name with name and Date with date
 letter = '''Dear <|Name|>,
                  you are selected!
                  <|Date|>'''
 
-print(letter.replace("<|Name|>", "Bismah").replace("<|Date|>","18 Feb 2025")) #chaining of .replace function
+print(letter.replace("<|Name|>", "Shine").replace("<|Date|>","18 Feb 2025")) #chaining of .replace function
