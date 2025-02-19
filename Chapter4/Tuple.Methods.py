b85bdfa71478c34d50d82de1ad74a398c8a22a30
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-19 10:31:27 +0500

    Update Tuple.Methods.py

--- Chapter4/Tuple.Methods.py
+++ Chapter4/Tuple.Methods.py
@@ -1,8 +1,17 @@
 a = ("hello", 1, 4.2,1, False, "Shine")
-print(a)
+# print(a)
 
 # to count the number of element appear in tuple 
 # print(a.count(1))
 
 # to check index of an element in a tuple
-print(a.index(1)) #it will give me the index of 1st occurence
+# print(a.index(1)) #it will give me the index of 1st occurence
+
+# packed tuple
+# a =  2, 3
+# print(a)
+
+# unpacked tuple
+my_tuple = (10, 20, 30)
+b, c, d = my_tuple
+print(b,c,d)
