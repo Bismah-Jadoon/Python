0bb264d6aa51182b2935149358d34a134281d294
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-24 10:16:52 +0500

    Add files via upload

--- Chapter2/Problems/problem1.py
+++ Chapter2/Problems/problem1.py
@@ -1,1 +1,8 @@
+# write a python program to add two numbers
+a = 1
 
+b = 2
+
+sum = a + b
+
+print("The sum of two number is : ", sum)
---
+++ Chapter2/Problems/problem3.py
@@ -0,0 +1,4 @@
+# check the type of variable assigned using input function
+
+a = input("give a value of a : ")
+print(type(a))
