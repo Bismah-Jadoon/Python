d1ebb788799a0c288f1b327169673fdc5149d3c5
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-24 10:16:07 +0500

    Delete Chapter2/problem5.py

--- Chapter2/problem5.py
+++
@@ -1,8 +0,0 @@
-# write a python program to find an average of two numbers entered by the user
-
-a = int(input("enter value of a : "))
-b = int(input("enter value of b : "))
-
-average = (a + b)/2
-
-print("the average of two numbers is : ", average)
