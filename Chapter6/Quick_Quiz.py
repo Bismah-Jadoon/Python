2dad00be032c60fc0176543645f8c2092494bbd3
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-24 11:47:08 +0500

    Add files via upload

---
+++ Chapter6/Quick_Quiz.py
@@ -0,0 +1,7 @@
+# Write a program to enter "yes" when the age entered by the user is greater than or equal to 18
+
+age = int(input("Enter your age: "))
+if(age >= 18):
+    print("Yes")
+
+print("End Program")
