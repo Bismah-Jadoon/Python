4d9fc725c111d67aebe9aacd54f68e3666d3471b
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-24 10:14:31 +0500

    Delete Chapter3/problem5.py

--- Chapter3/problem5.py
+++
@@ -1,7 +0,0 @@
-#Write a program to format the letter using escape sequence characters
-
-# letter = "Dear Harry, this python course is nice, Thanks!"
-
-letter = "Dear Harry,\n\t This python course is nice.\n\t Thanks!"
-print(letter)
-
