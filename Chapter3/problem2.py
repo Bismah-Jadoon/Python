42759ebaa2565db878764a096e2472688998a74b
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-24 10:14:03 +0500

    Delete Chapter3/problem2.py

--- Chapter3/problem2.py
+++
@@ -1,6 +0,0 @@
-# Replace Name with name and Date with date
-letter = '''Dear <|Name|>,
-                 you are selected!
-                 <|Date|>'''
-
-print(letter.replace("<|Name|>", "Shine").replace("<|Date|>","18 Feb 2025")) #chaining of .replace function
