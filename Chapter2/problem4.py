e8d922a3698f2cb63aea934a5b9a3d413c6e2a41
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-24 10:15:58 +0500

    Delete Chapter2/problem4.py

--- Chapter2/problem4.py
+++
@@ -1,5 +0,0 @@
-# use a comparison operator to find out whether a given variable 'a' is greater than 'b' or not.
-a = int(input("enter value of a : "))
-b = int(input("enter value of b : "))
-
-print("Value of a is greater than b : ", a > b)
