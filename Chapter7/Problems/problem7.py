6e14baf79bd078aa056036500928cbd81dcbc066
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-03-02 11:14:46 +0500

    Add files via upload

---
+++ Chapter7/Problems/problem7.py
@@ -0,0 +1,8 @@
+# write a program to print star pattern
+
+n = int(input("Enter a number : "))
+
+for i in range(1, n+1):
+    print(" " * (n-i), end="")
+    print("*"* (2*i - 1), end="" )
+    print("")
