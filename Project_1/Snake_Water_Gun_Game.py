9900ce880470eef33cadd56f36bca66c339e452e
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-03-03 16:39:46 +0500

    Update Snake_Water_Gun_Game.py

--- Project_1/Snake_Water_Gun_Game.py
+++ Project_1/Snake_Water_Gun_Game.py
@@ -1,4 +1,5 @@
 '''
+Game Rules:
 Snake vs. Water: Snake drinks the water hence wins.
 Water vs. Gun: The gun will drown in water, hence a point for water
 Gun vs. Snake: Gun will kill the snake and win.
