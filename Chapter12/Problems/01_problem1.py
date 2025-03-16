3310ebf8cb84419464a94d03f0befdad93b6bec5
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-03-16 10:38:33 +0500

    Add files via upload

--- Chapter12/Problems/01_problem1.py
+++ Chapter12/Problems/01_problem1.py
@@ -1,1 +1,7 @@
+# Write a program to open three files 1.txt, 2.txt and 3.txt if any these files are not present, a message without exiting the program must be printed prompting the same.
+with open("one.txt", "r") as f1, open("two.txt", "r") as f2, open("three.txt", "r") as f3:
+    content1 = f1.read()
+    content2 = f2.read()
+    content3 = f3.read()
 
+print(content1,content2,content3)
